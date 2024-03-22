0a47f6d1e05cf6fcf4afc70ad61403a32afb48fb
lihan <lihan@apache.org>
2024-03-22 10:28:08 +0800

    home page display json

--- script/build_registry.sh
+++ script/build_registry.sh
@@ -5,7 +5,9 @@ pluginsDir=$1
 #Rename plugin json file and remove the suffix .json
 for file in $pluginsDir/*.json; do
     if [ "$file" = "$pluginsDir/index.json" ]; then
+        mv "$file" "${file%.json}.html"
         continue
     fi
     mv "$file" "${file%.json}"
 done
+
